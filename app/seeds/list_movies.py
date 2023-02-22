26ede3d3468ce0119cec4581aed27301c257bb13
paul fixler <fixlerpaul@gmail.com>
2023-02-22 12:49:30 -0500

    completed database before testing

--- app/seeds/list_movies.py
+++ app/seeds/list_movies.py
@@ -1,9 +1,67 @@
-from app.models import db, Review, environment, SCHEMA
+from app.models import db, Movie, List, environment, SCHEMA
 
 
 def seed_list_movies():
-    list_movie1
+    movie1 = db.session.query(Movie).get(1)
+    movie2 = db.session.query(Movie).get(2)
+    movie3 = db.session.query(Movie).get(3)
+    movie4 = db.session.query(Movie).get(4)
+    movie5 = db.session.query(Movie).get(5)
+    movie6 = db.session.query(Movie).get(6)
+    movie7 = db.session.query(Movie).get(7)
+    movie8 = db.session.query(Movie).get(8)
+    movie9 = db.session.query(Movie).get(9)
+    movie10 = db.session.query(Movie).get(10)
+    movie11 = db.session.query(Movie).get(11)
+    movie12 = db.session.query(Movie).get(12)
+    movie13 = db.session.query(Movie).get(13)
+    movie14 = db.session.query(Movie).get(14)
+    movie15 = db.session.query(Movie).get(15)
+    movie16 = db.session.query(Movie).get(16)
 
+    list1 = db.session.query(List).get(1)
+    list2 = db.session.query(List).get(2)
+    list3 = db.session.query(List).get(3)
+    list4 = db.session.query(List).get(4)
+
+    list1.lists.append(movie1)
+    list1.lists.append(movie2)
+    list1.lists.append(movie3)
+    list1.lists.append(movie4)
+
+    list2.lists.append(movie5)
+    list2.lists.append(movie6)
+    list2.lists.append(movie7)
+    list2.lists.append(movie8)
+
+    list3.lists.append(movie9)
+    list3.lists.append(movie10)
+    list3.lists.append(movie11)
+    list3.lists.append(movie12)
+
+    list4.lists.append(movie13)
+    list4.lists.append(movie14)
+    list4.lists.append(movie15)
+    list4.lists.append(movie16)
+
+    movie1.movies.append(list1)
+    movie2.movies.append(list1)
+    movie3.movies.append(list1)
+    movie4.movies.append(list1)
+    movie5.movies.append(list2)
+    movie6.movies.append(list2)
+    movie7.movies.append(list2)
+    movie8.movies.append(list2)
+    movie9.movies.append(list3)
+    movie10.movies.append(list3)
+    movie11.movies.append(list3)
+    movie12.movies.append(list3)
+    movie13.movies.append(list4)
+    movie14.movies.append(list4)
+    movie15.movies.append(list4)
+    movie16.movies.append(list4)
+
+    db.session.commit()
 
 
 def undo_list_movies():
--- app/seeds/lists.py
+++ app/seeds/lists.py
@@ -1,9 +1,46 @@
-from app.models import db, Review, environment, SCHEMA
+from app.models import db, List, environment, SCHEMA
 
 
 def seed_lists():
-    list1
+    list1 = List(
+        user_id=1,
+        name='Cheese related movies',
+        description='This is a list of movies that are related to chesse in some capacity',
+        public_list=True,
+        created_at='2023-02-22',
+        updated_at='2023-02-22'
+    )
 
+    list2 = List(
+        user_id=2,
+        name='Movies with cheese in them',
+        description='This is a list of movies that have cheese in them',
+        public_list=True,
+        created_at='2023-02-22',
+        updated_at='2023-02-22'
+    )
+
+    list3 = List(
+        user_id=3,
+        name='I like cheese',
+        description='Cheese',
+        public_list=True,
+        created_at='2023-02-22',
+        updated_at='2023-02-22'
+    )
+
+    list4 = List(
+        user_id=3,
+        name='I am literally a mouse',
+        description='Squeak',
+        public_list=True,
+        created_at='2023-02-22',
+        updated_at='2023-02-22'
+    )
+
+    all_lists = [list1, list2, list3, list4]
+    add_lists = [db.session.add(list) for list in all_lists]
+    db.session.commit()
 
 
 def undo_lists():
