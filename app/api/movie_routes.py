80855d3ab28876cd65fbc040749de6ddbcf0cec0
paul fixler <fixlerpaul@gmail.com>
2023-02-23 12:02:35 -0500

    started

--- app/api/movie_routes.py
+++ app/api/movie_routes.py
@@ -6,18 +6,18 @@ movie_routes = Blueprint('movies', __name__)
 @movie_routes.route('/')
 def movies():
     """
-    Query for all movies and returns them in a list of movie dictionaries
+    Query for all movies and return them in a list of movie dictionaries
     """
 
     movies = Movie.query.all()
     return {'movies': [movie.to_dict() for movie in movies]}
 
 
-@movie_routes.route('/<int:movieId>')
-def movie(movieId):
+@movie_routes.route('/<int:movie_id>')
+def movie(movie_id):
     """
-    Query for a movie by id and returns that movie in a dictionary
+    Query for a movie by id and return that movie in a dictionary
     """
 
-    movie = Movie.query.get(movieId)
+    movie = Movie.query.get(movie_id)
     return movie.to_dict()
