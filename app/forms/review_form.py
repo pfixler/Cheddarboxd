4781619cd1172ca7a93483d3db44aaa7026f49ff
paul fixler <fixlerpaul@gmail.com>
2023-02-23 12:16:31 -0500

    review form created

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -3,4 +3,11 @@ from wtforms import StringField, FloatField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
 
-# class ReviewForm(FlaskForm):
+class ReviewForm(FlaskForm):
+    watched = BooleanField("watched", validators=[DataRequired()])
+    watch_date = StringField("watch_data")
+    rating = FloatField("rating")
+    like = BooleanField("like")
+    content = StringField("content", validators=[DataRequired()])
+    created_at = StringField("created_at", validators=[DataRequired()])
+    updated_at = StringField("updated_at")
