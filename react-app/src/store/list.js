5772004d18c4770bcbb7d3e8a949fc67c73bf0a5
paul fixler <fixlerpaul@gmail.com>
2023-02-24 11:48:51 -0500

    list thunks additions

--- react-app/src/store/list.js
+++ react-app/src/store/list.js
@@ -1,5 +1,5 @@
 
-const LOAD_ALL_LISTS = 'lists/LOAD_ALL_LISTS'
+const LOAD_ALL_LISTS = 'lists/LOAD_ALL_LISTS';
 const LOAD_MOVIE_LISTS = 'lists/LOAD_MOVIE_LISTS';
 const LOAD_USER_LISTS = 'lists/LOAD_USER_LISTS';
 const CREATE_LIST = 'list/CREATE_LIST';
@@ -35,3 +35,69 @@ const remove = (list) => ({
     type: DELETE_LIST,
     list
 })
+
+
+export const loadAllLists = () => async (dispatch) => {
+    const response = await fetch('/api/lists/');
+
+    if (response.ok) {
+        const lists = await response.json();
+        dispatch(loadAll(lists))
+    }
+}
+
+
+export const loadMovieLists = (movieId) => async (dispatch) => {
+    const response = await fetch(`/api/lists/movie/${movieId}`)
+
+    if (response.ok) {
+        const movieLists = await response.json();
+        dispatch(loadMovie(movieLists))
+    }
+}
+
+export const loadUserLists = (userId) => async (dispatch) => {
+    const response = await fetch(`/api/lists/user/${userId}`)
+
+    if (response.ok) {
+        const userLists = await response.json();
+        dispatch(loadUser(userLists))
+    }
+}
+
+export const createList = (list) => async (dispatch) => {
+    const response = await fetch(`/api/lists/`, {
+        method:"POST",
+        headers:{ 'Content-Type': 'application/json' },
+        body: JSON.stringify(list),
+    })
+    if (response.ok) {
+        const newList = await response.json()
+        dispatch(create(newList))
+    }
+}
+
+export const updateList = (list) => async (dispatch) => {
+    const response = await fetch(`/api/lists/${list.id}`, {
+        method:"PUT",
+        headers:{ 'Content-Type': 'application/json' },
+        body: JSON.stringify(list),
+    })
+    if (response.ok) {
+        const updatedList = await response.json()
+        dispatch(update(updatedList))
+    }
+}
+
+export const deleteList = (list) => async (dispatch) => {
+    const response = await fetch(`/api/lists/${list.id}`, {
+        method:"DELETE"
+    })
+
+    if (response.ok) {
+        dispatch(remove(list))
+    }
+}
+
+
+// const initialState = {  }
