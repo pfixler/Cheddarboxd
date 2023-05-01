f11d2111687031c81151db7648084450acb445b8
paul fixler <fixlerpaul@gmail.com>
2023-04-30 23:49:05 -0400

    edit profile page jsx partial

--- react-app/src/components/ProfileSettings/index.js
+++ react-app/src/components/ProfileSettings/index.js
@@ -3,7 +3,58 @@
 
 const ProfileSettings = () => {
     return (
-        <div>
+        <div className="profile-settings-box">
+            <div className="profile-settings-header-box">
+                <h1 className="profile-settings-header">
+                    Account Settings
+                </h1>
+            </div>
+            <div className="profile-settings-navbar">
+                <ul className="profile-settings-navbar-list">
+                    <li className="profile-settings-navbar-list-item">
+
+                    </li>
+                    <li className="profile-settings-navbar-list-item">
+
+                    </li>
+                </ul>
+            </div>
+            <div className="profile-settings-form-box">
+                <div className="profile-settings-form-header-box">
+                    <h2 className="profile-settings-form-header">
+                        Profile
+                    </h2>
+                </div>
+                <form className="profile-settings-form">
+                    <div className="personal-settings">
+
+                    </div>
+                    <div className="favorite-films">
+                        <div className="favorite-films-header">
+                            <label className="favorite-films-label">
+                                Favorite Films
+                            </label>
+                            <p className="favorite-films-instructions">
+                                Drag posters to reorder.
+                            </p>
+                        </div>
+                        <ul className='favorite-films-poster-list'>
+                            <li className='favorite-films-poster-list-placeholder'>
+
+                            </li>
+                            <li className='favorite-films-poster-list-placeholder'>
+
+                            </li>
+                            <li className='favorite-films-poster-list-placeholder'>
+
+                            </li>
+                            <li className='favorite-films-poster-list-placeholder'>
+
+                            </li>
+                        </ul>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
