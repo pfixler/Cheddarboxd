7e49ed429102ea8cacd27545ef358635628cc589
paul fixler <fixlerpaul@gmail.com>
2023-05-06 23:09:53 -0400

    edit profile route and thunk

--- react-app/src/store/profile.js
+++ react-app/src/store/profile.js
@@ -1,10 +1,16 @@
 const LOAD_PROFILE_DETAILS = 'profile/LOAD_PROFILE_DETAILS';
+const UPDATE_PROFILE = 'profile/UPDATE_PROFILE';
 const FOLLOW_PROFILE = 'profile/FOLLOW_PROFILE';
 const UNFOLLOW_PROFILE = 'profile/UNFOLLOW_PROFILE';
 
 
 const loadProfile = (profile) => ({
     type: LOAD_PROFILE_DETAILS,
+    profile
+})
+
+const update = (profile) => ({
+    type: UPDATE_PROFILE,
     profile
 })
 
@@ -28,6 +34,19 @@ export const loadProfileDetails = (profileId) => async (dispatch) => {
     }
 }
 
+export const updateProfile = (profile) => async (dispatch) => {
+    const response = await fetch(`/api/profiles/${profile.id}`, {
+        method:"PUT",
+        headers:{ 'Content-Type': 'application/json' },
+        body: JSON.stringify(profile),
+    })
+
+    if (response.ok) {
+        const updatedProfile = await response.json()
+        dispatch(update(updatedProfile))
+    }
+}
+
 
 const initialState = { profile: {} }
 
@@ -37,6 +56,10 @@ const profile = (state = initialState, action) => {
         case LOAD_PROFILE_DETAILS:
             newState = {...state, profile: {}};
             newState.profile = action.profile;
+            return newState;
+        case UPDATE_PROFILE:
+            newState = {profile: {...state.profile}};
+            newState.profile[action.profile.id] = action.profile;
             return newState;
         default:
             return state;
