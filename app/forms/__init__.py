828cb1cf497006607cb0b9c5bc31c14e21f9f3ac
paul fixler <fixlerpaul@gmail.com>
2023-05-06 22:41:28 -0400

    edit profile form complete

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .review_form import ReviewForm
 from .list_form import ListForm
+from .signup_form import user_exists, is_email, password_length, username_exists
--- app/forms/edit_profile_form.py
+++ app/forms/edit_profile_form.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
+from app.forms import user_exists, is_email, password_length, username_exists
 
 class EditProfileForm(FlaskForm):
-    StringField("name", validators=[DataRequired()])
+    username = StringField("username", validators=[DataRequired(), username_exists])
+    email = StringField("email", validators=[DataRequired(), user_exists, is_email])
+    first_name = StringField("first_name")
+    last_name = StringField("last_name")
+    location = StringField("location")
+    website = StringField("website")
+    bio = StringField("bio")
