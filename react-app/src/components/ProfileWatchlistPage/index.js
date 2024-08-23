49ceec58961e9ba763225810f77cf74a4e5eab36
paul fixler <fixlerpaul@gmail.com>
2024-08-23 08:14:22 -0400

    profile watchlist navbar

--- react-app/src/components/ProfileWatchlistPage/index.js
+++ react-app/src/components/ProfileWatchlistPage/index.js
@@ -5,10 +5,47 @@ const ProfileWatchlistPage = () => {
 
     return (
         <div>
-
-
-
-        
+            <nav className="profile-navbar">
+                <ul className="profile-navbar-list">
+                    <li className='navitem'>
+                        {/* <NavLink exact to={`/profiles/${sessionUser.id}`}
+                            className="profile-navbar-list-item profile-link"
+                        >
+                            Profile
+                        </NavLink> */}
+                    </li>
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Activity</NavLink>
+                    </li> */}
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Films</NavLink>
+                    </li> */}
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Diary</NavLink>
+                    </li> */}
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Reviews</NavLink>
+                    </li> */}
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Watchlist</NavLink>
+                    </li> */}
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Lists</NavLink>
+                    </li> */}
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Likes</NavLink>
+                    </li> */}
+                    {/* <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Tags</NavLink>
+                    </li>
+                    <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Network</NavLink>
+                    </li>
+                    <li className='navitem'>
+                        <NavLink exact to="/" className="profile-navbar-list-item">Invitations</NavLink>
+                    </li> */}
+                </ul>
+            </nav>
         </div>
     )
 }
